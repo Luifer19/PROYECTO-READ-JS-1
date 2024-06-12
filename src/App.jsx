c4f10acea61550e56cb62f26c109b010ce8292c1
luisa gomez <luisafernandauribegomez11@gmail.com>
2024-06-12 02:09:15 -0500

    Se creo registro, visualización y modulación de proyecto.

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,55 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Conversor from './conversor'
+import Usuarios from './Usuarios'
+import Registro from './Registro'
 
 function App() {
   const [usuario, setUsuario] = useState('')
   const [clave, setClave] = useState('')
   const [logueado, setLogueado] = useState(false)
+  const [recargar, setRecargar] = useState(false)
 
   function cambiarUsuario(evento) {
     setUsuario(evento.target.value)
-
   }
+
   function cambiarClave(evento) {
     setClave(evento.target.value)
-
   }
+  function recargarAhora() {
+    setRecargar(!recargar)
+  }
+
   async function ingrsar() {
-    const peticion = await fetch('http://localhost:3000/login?usuario='+usuario+'&clave='+clave,{credentials:'include'}) 
-    if(peticion.ok){
+    const peticion = await fetch('http://localhost:3000/login?usuario=' + usuario + '&clave=' + clave, { credentials: 'include' })
+    if (peticion.ok) {
       setLogueado(true)
-    }else{
+
+    } else {
       alert('usuario o clave incorrectos')
     }
-    // if (usuario == 'admin' && clave == 'admin') {
-    //  alert('ingresaste')
-    //   setLogueado(true)
-    // } else {
-    //  alert('usuario o clave incorrectos ')
-    //}
   }
   async function validar() {
-    const peticion = await fetch('http://localhost:3000/validar',{credentials:'include'}) 
-    if(peticion.ok){
+    const peticion = await fetch('http://localhost:3000/validar', { credentials: 'include' })
+    if (peticion.ok) {
       setLogueado(true)
-    }else{
+
     }
-    
   }
-  useEffect(()=> {
-validar()
-  },[])
+
+  useEffect(() => {
+    validar()
+  }, [])
 
   if (logueado) {
-    return <Conversor />
+    return (
+      <>
+        <Conversor />
+        <Registro recargarAhora={recargarAhora} />
+        <Usuarios recargar={recargar} />
+
+      </>)
   }
   return (
     <>
@@ -52,6 +57,9 @@ validar()
       <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
       <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
       <button onClick={ingrsar}>Ingresar </button>
+
+
+
     </>
   )
 }
