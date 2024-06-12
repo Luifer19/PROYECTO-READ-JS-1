e0ed9d5a628a9b44296624f8266a4dffd748cbe3
luisa gomez <luisafernandauribegomez11@gmail.com>
2024-06-12 02:10:26 -0500

    Se creo registro, visualización y modulacion de proyecto

---
+++ src/Registro.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function Registro({ recargarAhora }) {
+  const [usuarioRegistro, setUsuarioRegistro] = useState('')
+  const [claveRegistro, setClaveRegistro] = useState('')
+
+  function cambiarUsuarioRegistro(evento) {
+    setUsuarioRegistro(evento.target.value)
+  }
+
+  function cambiarClaveRegistro(evento) {
+    setClaveRegistro(evento.target.value)
+  }
+
+  async function registrar() {
+    const peticion = await fetch('http://localhost:3000/registro?usuario=' + usuarioRegistro + '&clave=' + claveRegistro, { credentials: 'include' })
+    if (peticion.ok) {
+      alert("usuario registrado")
+      recargarAhora()
+    } else {
+      alert('usuario no registrado')
+    }
+  }
+
+  useEffect(() => {
+  }, [])
+
+  return (
+    <>
+      <h1>Registro</h1>
+      <input placeholder='Usuario' type="text" name="usuario" id="usuario" value=
+        {usuarioRegistro} onChange={cambiarUsuarioRegistro} />
+      <input placeholder='Clave' type="password" name="clave" id="clave" value=
+        {claveRegistro} onChange={cambiarClaveRegistro} />
+      <button onClick={registrar}>Registrar </button>
+    </>
+  )
+}
+export default Registro
---
+++ src/Usuarios.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function Usuarios(recargar) {
+  const [usuarios, setUsuarios] = useState([])
+
+  async function obtenerUsuarios() {
+    const peticion = await fetch('http://localhost:3000/usuarios', { credentials: 'include' })
+    if (peticion.ok) {
+      const respuesta = await peticion.json()
+      setUsuarios(respuesta)
+    }
+  }
+
+  async function eliminarUsuario(id) {
+    const peticion = await fetch('http://localhost:3000/usuarios?id=' + id, {
+      credentials:
+        'include', method: 'DELETE'
+    })
+    if (peticion.ok) {
+      alert('usuario eliminado')
+      obtenerUsuarios();
+
+    }
+  }
+  useEffect(() => {
+    obtenerUsuarios()
+  }, [recargar])
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Usuario</th>
+            <th>Clave</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            usuarios.map(usuario => (
+              <tr key={usuario.id}>
+                <th>{usuario.id}</th>
+                <th>{usuario.usuario}</th>
+                <th>{usuario.clave}</th>
+                <th>
+                  <button
+                    onClick={() => { eliminarUsuario(usuario.id) }}
+                  >X</button>
+                </th>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
+  )
+}
+export default Usuarios 
